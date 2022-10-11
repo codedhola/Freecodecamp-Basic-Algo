cdc146fbc99108318155edf6881528d310def7e1
codedhola <codedhola2000@gmail.com>
2022-10-11 15:36:44 +0100

    FEAT: added index-tO-Insert algorithm

---
+++ indexToInsert.js
@@ -0,0 +1,60 @@
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+
+function getIndexToIns(arr, num) {
+    arr.push(num);
+    const sort = arr.sort((a, b) => a - b);
+    const index = arr.indexOf(num);
+    return index;
+  }
+  
+  getIndexToIns([40, 60], 50);
+  
+  
+  // FREECODECAMP
+  
+  // function getIndexToIns(arr, num) {
+  //   arr.sort((a, b) => a - b);
+  
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] >= num)
+  //       return i;
+  //   }
+  
+  //   return arr.length;
+  // }
+  
+  
+  
+  // 2
+  
+  // function getIndexToIns(arr, num) {
+  //   return arr
+  //     .concat(num)
+  //     .sort((a, b) => a - b)
+  //     .indexOf(num);
+  // }
+  
+  // getIndexToIns([1, 3, 4], 2);
+  
+  
+  
+  
+  // 3
+  
+  // function getIndexToIns(arr, num) {
+  //   arr.sort((a, b) => a - b);
+  
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] >= num)
+  //       return i;
+  //   }
+  
+  //   return arr.length;
+  // }
+  
+  
