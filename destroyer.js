0244b750f642171b847287c4ae56994668a5a217
codedhola <codedhola2000@gmail.com>
2022-11-01 00:23:13 +0100

    solved destroyer algorithm

---
+++ destroyer.js
@@ -0,0 +1,32 @@
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note: You have to use the arguments object.
+
+function destroyer(arr) {
+    const arg = [];
+    const result = []
+    for(let i = 1;i < arguments.length;i++){
+      arg.push(arguments[i]);
+    }
+    for(let i = 0;i < arr.length;i++){
+      if(arg.includes(arr[i]) === false){
+        result.push(arr[i])
+      }
+    }
+    return result;
+  }
+  
+ destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+
+// FREECODECAMP HACK
+// function destroyer(arr) {
+//     const valsToRemove = Array.from(arguments).slice(1);
+//     return arr.filter(function(val) {
+//       return !valsToRemove.includes(val);
+//     });
+//   }
+
+// function destroyer(arr, ...valsToRemove) {
+//     return arr.filter(elem => !valsToRemove.includes(elem));
+//   }
