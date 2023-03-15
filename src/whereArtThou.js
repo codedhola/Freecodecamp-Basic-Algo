c53a09e095597f28ea537e6353f0d4f01cca0f11
codedhola <codedhola2000@gmail.com>
2023-03-15 19:32:47 +0100

    fix: improved whereArtThou algo

--- src/whereArtThou.js
+++ src/whereArtThou.js
@@ -1,4 +1,20 @@
-// ONLY SOLUTION 
+/**
+ * 
+ * // QUESTION
+ * 
+ * 
+
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+ * 
+ * 
+ */
+
+
+
+
 function whatIsInAName(collection, source) {
     const souceKeys = Object.keys(source);
   
@@ -22,3 +38,30 @@ function whatIsInAName(collection, source) {
     ],
     { last: "Capulet" }
   );
+
+
+
+  // LOVED SOLUTION
+  /* 
+
+  function whatIsInAName(collection, source) {
+  // What's in a name?
+  const arr = [];
+  // Only change code below this line
+  for (let i = 0; i < collection.length; i++) {
+    let found = true;
+    for (const sourceProp in source) {
+      if (collection[i][sourceProp] !== source[sourceProp]) {
+        found = false;
+        break;
+      }
+    }
+    if (found) arr.push(collection[i]);
+  }
+  // Only change code above this line
+  return arr;
+}
+
+
+
+  */
